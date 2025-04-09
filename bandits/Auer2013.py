04b2ebacd88eafe16911718dbefda4440a837919
LennertSaerens <lennert.saerens@gmail.com>
2025-04-09 14:27:01 +0200

    Fixed bugs in set operations and termination

--- bandits/Auer2013.py
+++ bandits/Auer2013.py
@@ -30,7 +30,8 @@ class Auer2013Bandit:
         self.optimal = []
 
         self.arm_means = np.zeros((num_arms, num_objectives))
-        self.arm_counts = np.zeros((num_arms, num_objectives))
+        self.itt_arm_counts = np.zeros((num_arms, num_objectives))
+        self.total_arm_counts = np.zeros((num_arms, num_objectives))
         self.current_arm = 0
 
     def choose_arm(self):
@@ -39,13 +40,22 @@ class Auer2013Bandit:
 
         :return: The arm to pull.
         """
+        if len(self.active_arms) == 0:
+            # If there are no active arms left, return 0
+            return 0
+
         # Check if all active arms have been pulled once
-        if np.all(self.arm_counts[self.active_arms] > 0):
+        if np.all(self.itt_arm_counts[self.active_arms] > 0):
             # If all active arms have been pulled once, update the optimal and active arms
             self.update_optimal_and_active_arms()
 
+        if len(self.active_arms) == 0:
+            # If there are no active arms left, return 0
+            return 0
+
         arm = self.active_arms[self.current_arm]
-        self.arm_counts[arm] += 1
+        self.itt_arm_counts[arm] += 1
+        self.total_arm_counts[arm] += 1
         self.current_arm = (self.current_arm + 1) % len(self.active_arms)
         return arm
 
@@ -62,43 +72,71 @@ class Auer2013Bandit:
 
         :return: None
         """
+        # print(f"Arm means:\n {self.arm_means}")
+        # print(f"Total arm counts:\n {self.total_arm_counts}")
         # Get the active_arms arms i for which the min_gap between the arm and all other active_arms arms j is
         # lesser than or equal to CI(i) + CI(j)
-        A_1 = np.where(
-            np.any(
-                np.array([
-                    self.get_min_gap(i, j) <= self.get_arm_ci(i) + self.get_arm_ci(j)
-                    for i in self.active_arms
-                    for j in self.active_arms
-                ]), axis=1
-            )
-        )[0]
+        A_1 = []
+        for i in self.active_arms:
+            valid = True
+            for j in self.active_arms:
+                if i != j:
+                    min_gap = self.get_min_gap(i, j)
+                    ci_sum = (self.get_arm_ci(i) + self.get_arm_ci(j))[0]
+                    # print(f"min_gap({i}, {j}): {min_gap}, CI({i}) + CI({j}): {ci_sum}")
+                    if min_gap > ci_sum:
+                        valid = False
+                        break
+            if valid:
+                A_1.append(i)
+
+        # print(f"A_1: {A_1}")
+
         # Get the arms i from A_1 \ {i} for which the max_gap between the arm and all other active_arms arms j is
         # greater than or equal to CI(i) + CI(j)
-        P_1 = np.where(
-            np.any(
-                np.array([
-                    self.get_max_gap(i, j) >= self.get_arm_ci(i) + self.get_arm_ci(j)
-                    for i in A_1
-                    for j in np.setdiff1d(A_1, i)
-                ]), axis=1
-            )
-        )[0]
+        P_1 = []
+        for i in A_1:
+            valid = True
+            for j in A_1:
+                if i != j:
+                    max_gap = self.get_eps_max_gap(i, j)
+                    ci_sum = (self.get_arm_ci(i) + self.get_arm_ci(j))[0]
+                    # print(f"max_gap({i}, {j}): {max_gap}, CI({i}) + CI({j}): {ci_sum}")
+                    if max_gap < ci_sum:
+                        valid = False
+                        break
+            if valid:
+                P_1.append(i)
+
+        # print(f"P_1: {P_1}")
+
         # Get the arms j from P_1 for which there exists no arm i in A_1 \ P_1 such that the max_gap
         # between i and j is less than or equal to CI(i) + CI(j)
-        P_2 = np.where(
-            np.all(
-                np.array([
-                    self.get_max_gap(i, j) <= self.get_arm_ci(i) + self.get_arm_ci(j)
-                    for i in np.setdiff1d(A_1, P_1)
-                    for j in P_1
-                ]), axis=1
-            )
-        )[0]
+        P_2 = []
+        for j in P_1:
+            valid = True
+            for i in np.setdiff1d(A_1, P_1):
+                if i != j:
+                    max_gap = self.get_eps_max_gap(i, j)
+                    ci_sum = (self.get_arm_ci(i) + self.get_arm_ci(j))[0]
+                    # print(f"max_gap({i}, {j}): {max_gap}, CI({i}) + CI({j}): {ci_sum}")
+                    if max_gap <= ci_sum:
+                        valid = False
+                        break
+            if valid:
+                P_2.append(j)
+
+        # print(f"P_2: {P_2}")
+
         # Set the active_arms arms to the difference between A_1 and P_2
         self.active_arms = np.setdiff1d(A_1, P_2)
+        # print(f"New active_arms: {self.active_arms}")
         # Set the optimal arms to the union of the current optimal arms and P_2
         self.optimal = np.union1d(self.optimal, P_2)
+        # print(f"New optimal arms: {self.optimal}")
+
+        # Reset the arm counts
+        self.itt_arm_counts[self.active_arms] = 0
         # Reset the current arm to 0
         self.current_arm = 0
 
@@ -109,11 +147,12 @@ class Auer2013Bandit:
         :param arm: The arm to calculate the confidence interval for.
         :return: The confidence interval for the arm.
         """
-        return np.sqrt(
+        ci = np.sqrt(
             (2 * np.log(
-                4 * self.num_arms * self.num_objectives * self.arm_counts[arm] ** 2 / self.delta
-            )) / self.arm_counts[arm]
+                4 * self.num_arms * self.num_objectives * self.total_arm_counts[arm] ** 2 / self.delta
+            )) / self.total_arm_counts[arm]
         )
+        return ci
 
     def get_min_gap(self, arm_i: int, arm_j: int):
         """
@@ -124,9 +163,9 @@ class Auer2013Bandit:
         :return: The gap between the two arms.
         """
         # Find the smallest difference between the two arms across all objectives
-        return max(0, np.min(self.arm_means[arm_i] - self.arm_means[arm_j]))
+        return max(0, np.min(self.arm_means[arm_j] - self.arm_means[arm_i]))
 
-    def get_max_gap(self, arm_i: int, arm_j: int):
+    def get_eps_max_gap(self, arm_i: int, arm_j: int):
         """
         Calculate the maximum gap between two arms.
 
@@ -134,7 +173,7 @@ class Auer2013Bandit:
         :param arm_j: The second arm.
         :return: The maximum gap between the two arms.
         """
-        return max(0, np.max(self.arm_means[arm_i] - self.arm_means[arm_j]))
+        return max(0, np.max((self.arm_means[arm_i] + self.epsilon) - self.arm_means[arm_j]))
 
     def get_top_arms(self):
         return self.optimal
@@ -148,14 +187,15 @@ class Auer2013Bandit:
         :return: None
         """
         # Update the arm means based on the reward received
-        self.arm_means[arm] += (reward - self.arm_means[arm]) / self.arm_counts[arm]
+        self.arm_means[arm] += (reward - self.arm_means[arm]) / self.total_arm_counts[arm]
 
     def reset(self):
         """
         Reset the bandit instance to its initial state.
         """
         self.arm_means = np.zeros((self.num_arms, self.num_objectives))
-        self.arm_counts = np.zeros((self.num_arms, self.num_objectives))
+        self.itt_arm_counts = np.zeros((self.num_arms, self.num_objectives))
+        self.total_arm_counts = np.zeros((self.num_arms, self.num_objectives))
         self.active_arms = np.arange(self.num_arms)
         self.optimal = []
         self.current_arm = 0
