3cf8b915c4eba472d435b16fdc350568ba10a8fd
LennertSaerens <lennert.saerens@gmail.com>
2025-10-15 12:47:19 +0200

    Added molecules environment to the experiments.

--- FixBudExpSetup.py
+++ FixBudExpSetup.py
@@ -1,15 +1,15 @@
 from bandits.EGE import EGE
 from bandits.TTPFTS import NormalTTPFTSBandit
+from bandits.UCB import PUCB1Bandit
 from bandits.Uniform import UniformBandit
-
-from environments import N50VS, EgeExp1, EgeExp2, EgeExp3, EgeExp4, CovBoost
+from environments import Molecules
 
 
 def run_EGE_experiment(num_runs, max_budget, environment, results_file=None, write=True):
     for experiment in range(num_runs):
         print(f"Running EGE experiment {experiment + 1}/{num_runs}...")
 
-        for budget in range(91, max_budget + 1):
+        for budget in range(201, max_budget + 1):
             # Initialize the bandit with the current budget
             bandit = EGE(environment.num_arms, environment.num_objectives, budget)
 
@@ -32,73 +32,59 @@ def run_EGE_experiment(num_runs, max_budget, environment, results_file=None, wri
                     file.write(f"EGE,{experiment},{budget},{bernoulli_metric},{jaccard_metric},{mis_id_metric}\n")
 
 
-def run_TTPFTS_experiment(num_runs, max_budget, environment, results_file=None, write=True):
-    for experiment in range(num_runs):
-        print(f"Running TTPFTS experiment {experiment + 1}/{num_runs}...")
-        bandit = NormalTTPFTSBandit(environment.num_arms, environment.num_objectives, p=0.5)
+def run_anytime_experiment(num_runs, max_budget, environment, results_file=None, write=True):
+    algorithms = {
+        "Uniform": UniformBandit(environment.num_arms, environment.num_objectives),
+        "TTPFTS": NormalTTPFTSBandit(environment.num_arms, environment.num_objectives, p=0.5),
+        "PUCB1": PUCB1Bandit(environment.num_arms, environment.num_objectives, kappa=1),
+    }
 
-        for t in range(91, max_budget + 1):
-            arm = bandit.choose_arm()
-            reward = environment.pull_arm(arm)
-            bandit.learn(arm, reward)
+    for algorithm_name, bandit in algorithms.items():
+        for experiment in range(num_runs):
+            print(f"Running {algorithm_name} experiment {experiment + 1}/{num_runs}...")
+            bandit.reset()
 
-            recommended_arms = bandit.get_top_arms()
+            for t in range(201, max_budget + 1):
+                arm = bandit.choose_arm()
+                reward = environment.pull_arm(arm)
+                bandit.learn(arm, reward)
 
-            # Calculate the metrics
-            bernoulli_metric = environment.bernoulli_metric(recommended_arms)
-            jaccard_metric = environment.jaccard_metric(recommended_arms)
-            mis_id_metric = environment.mis_id_metric(recommended_arms)
+                recommended_arms = bandit.get_top_arms()
 
-            if results_file is not None and write:
-                with open(results_file, "a") as file:
-                    file.write(f"TTPFTS,{experiment},{t},{bernoulli_metric},{jaccard_metric},{mis_id_metric}\n")
+                # Calculate the metrics
+                bernoulli_metric = environment.bernoulli_metric(recommended_arms)
+                jaccard_metric = environment.jaccard_metric(recommended_arms)
+                mis_id_metric = environment.mis_id_metric(recommended_arms)
 
-
-def run_baseline_experiment(num_runs, max_budget, environment, results_file=None, write=True):
-    for experiment in range(num_runs):
-        print(f"Running Uniform Sampling experiment {experiment + 1}/{num_runs}...")
-        bandit = UniformBandit(environment.num_arms, environment.num_objectives)
-
-        for t in range(91, max_budget + 1):
-            arm = bandit.choose_arm()
-            reward = environment.pull_arm(arm)
-            bandit.learn(arm, reward)
-
-            recommended_arms = bandit.get_top_arms()
-
-            # Calculate the metrics
-            bernoulli_metric = environment.bernoulli_metric(recommended_arms)
-            jaccard_metric = environment.jaccard_metric(recommended_arms)
-            mis_id_metric = environment.mis_id_metric(recommended_arms)
-
-            if results_file is not None and write:
-                with open(results_file, "a") as file:
-                    file.write(f"Uniform,{experiment},{t},{bernoulli_metric},{jaccard_metric},{mis_id_metric}\n")
+                if results_file is not None and write:
+                    with open(results_file, "a") as file:
+                        file.write(f"{algorithm_name},{experiment},{t},{bernoulli_metric},{jaccard_metric},{mis_id_metric}\n")
 
 
 if __name__ == "__main__":
     # Set the parameters for the experiments
     num_runs = 100
-    max_budget = 2500
-
-    environments = {
-        # "EgeExp1": EgeExp1.EgeExp1(),
-        # "EgeExp2": EgeExp2.EgeExp2(),
-        # "EgeExp3": EgeExp3.EgeExp3(),
-        # "EgeExp4": EgeExp4.EgeExp4(),
-        # "N50VS": N50VS.N50VS(),
-        "CovBoost": CovBoost.CovBoost()
-    }
     write = True
+    # plt.rcParams.update({'font.family': 'serif'})
+    # plt.rcParams.update({'font.size': 15})
+    environments = {
+        # "EgeExp1": {"environment": EgeExp1.EgeExp1(), "budget": 3000},
+        # "EgeExp2": {"environment": EgeExp2.EgeExp2(), "budget": 2000},
+        # "EgeExp3": {"environment": EgeExp3.EgeExp3(), "budget": 3000},
+        # "EgeExp4": {"environment": EgeExp4.EgeExp4(), "budget": 3000},
+        # "EgeExp5": {"environment": EgeExp5.EgeExp5(), "budget": 3000},
+        # "EgeExp6": {"environment": EgeExp6.EgeExp6(), "budget": 3000},
+        # "EgeExp7": {"environment": EgeExp7.EgeExp7(), "budget": 3000},
+        # "EgeExp8": {"environment": EgeExp8.EgeExp8(), "budget": 3000},
+        # "N50VS": {"environment": N50VS.N50VS(), "budget": 3000},
+        # "CovBoost": {"environment": CovBoost.CovBoost(), "budget": 2500},
+        "Molecules": {"environment": Molecules.Molecules(num_arms=100), "budget": 10_000},
+    }
 
-    for environment_name, environment in environments.items():
+    for environment_name, env_dict in environments.items():
         print(f"\nRunning experiments for {environment_name}...")
-        results_file = f"results/TTPFTSvsUniform_{environment_name}.csv"
-        run_EGE_experiment(num_runs, max_budget, environment, results_file, write)
-        run_TTPFTS_experiment(num_runs, max_budget, environment, results_file, write)
-        run_baseline_experiment(num_runs, max_budget, environment, results_file, write)
-
-    # Run the experiments
-    # run_EGE_experiment(num_runs, max_budget, environment, results_file, write)
-    # run_TTPFTS_experiment(num_runs, max_budget, environment, results_file, write)
-    # run_baseline_experiment(num_runs, max_budget, environment, results_file, write)
+        results_file = f"results/EGEvsTTPFTSvsPUCB1vsUniform_{environment_name}.csv"
+        environment = env_dict["environment"]
+        max_budget = env_dict["budget"]
+        # run_EGE_experiment(num_runs, max_budget, environment, results_file=results_file, write=write)
+        run_anytime_experiment(num_runs, max_budget, environment, results_file=results_file, write=write)
